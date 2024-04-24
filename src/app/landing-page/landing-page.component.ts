bcc4912bc271ba336a5b8a91acae0ad1082a0a03
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-04-24 12:34:53 +0200

    about me

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AotComponent } from './aot/aot.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, AotComponent],
+  imports: [CommonModule, AotComponent, AboutMeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })

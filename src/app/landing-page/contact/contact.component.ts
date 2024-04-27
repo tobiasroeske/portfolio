26567be7eb851db71aab5b33edcd508552f38c15
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-04-27 11:59:37 +0200

    contact problem

--- src/app/landing-page/contact/contact.component.ts
+++ src/app/landing-page/contact/contact.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  labelHidden:boolean = true;
 
+  showLabel() {
+    this.labelHidden = false;
+  }
+
+  hideLabel() {
+    this.labelHidden = true;
+  }
+
+  stopPropagation(event:Event) {
+    event.stopPropagation();
+  }
 }

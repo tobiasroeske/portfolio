57f6abd368f0f0bd20cd1642120bedd7bd3ae937
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-04-27 11:34:59 +0200

    contact problem

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,15 +7,16 @@ import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HeaderServiceService } from '../shared/services/header-service/header-service.service';
 import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
+import { ContactComponent } from "./contact/contact.component";
 
 
 
 @Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [CommonModule, AotComponent, AboutMeComponent, ArrowComponent, MySkillsComponent, PortfolioComponent, DropdownMenuComponent],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+    selector: 'app-landing-page',
+    standalone: true,
+    templateUrl: './landing-page.component.html',
+    styleUrl: './landing-page.component.scss',
+    imports: [CommonModule, AotComponent, AboutMeComponent, ArrowComponent, MySkillsComponent, PortfolioComponent, DropdownMenuComponent, ContactComponent]
 })
 export class LandingPageComponent {
   headerDropDownService = inject(HeaderServiceService);

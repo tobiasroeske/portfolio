0472d18125e9b09c27d3521c3b157d8e7c093526
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-04-29 16:53:57 +0200

    responsive < 430

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, FooterComponent ,ImpressumComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Tobias Roeske';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
+    { path: 'impressum', component: ImpressumComponent },
 ];

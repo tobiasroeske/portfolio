34f103a77bed1de641243241f0f68a718d6ee265
Tobias Roeske <122990092+tobiasroeske@users.noreply.github.com>
2024-04-30 10:35:05 +0200

    about me real

--- src/app/shared/services/header-service/header-service.service.ts
+++ src/app/shared/services/header-service/header-service.service.ts
@@ -6,7 +6,7 @@ import { Subject } from 'rxjs';
 })
 export class HeaderServiceService {
   headerDropDownState$ = new Subject();
-  stateOfDropDown = true;
+  stateOfDropDown = false;
 
   constructor() {
     this.headerDropDownState$.subscribe(value => {
